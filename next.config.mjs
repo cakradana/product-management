534064d3213828005e315cd4cd086ca3ca1bb66b
cakradana <rcakradana@gmail.com>
2025-05-31 19:21:38 +0700

    Add Dockerfile for Next.js application setup and update next.config.mjs for standalone output

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "standalone",
   images: {
     remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'raw.githubusercontent.com'
-    }
-    ]
-  }
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
